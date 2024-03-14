5dc80b3b86a44a93d5ea97b49046bbdadebdb8a6
Kata Nagy <nagykata98@gmail.com>
2024-03-14 16:56:24 +0000

    Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-// import React from "react";
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Szotyi",
-  //     animal: "Guinea Pig",
-  //     breed: "Common",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Charlie",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]);
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Szotyi" animal="Guinea Pig" breed="Common" />
-    <Pet name="Charlie" animal="Cat" breed="Mixed" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
